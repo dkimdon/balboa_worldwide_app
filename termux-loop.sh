20e6c997a886b624a589c9f23092b7a493137763
David Kimdon <dkimdon@gmail.com>
2020-01-13 22:28:37 -0800

    Kill the client if it runs for more than 20 seconds.  Change the refresh interval to every two minutes.

--- termux-loop.sh
+++ termux-loop.sh
@@ -4,7 +4,7 @@ termux-wake-lock
 
 . /data/data/com.termux/files/home/hottub-aws.sh
 
-while sleep 60; do
-/data/data/com.termux/files/home/balboa_worldwide_app/bin/bwa_aws_iot_thing \
+while sleep 120; do
+timeout --signal=9 20 /data/data/com.termux/files/home/balboa_worldwide_app/bin/bwa_aws_iot_thing \
 	| tee -a  /data/data/com.termux/files/home/bwa_aws_iot_thing.log 2>&1
 done
