94f4be75225d06b9cc6cc5e7c8e489e051545ca9
David Kimdon <dkimdon@gmail.com>
2020-02-02 08:47:35 -0800

    Switch to do-while flavor.

--- termux-loop.sh
+++ termux-loop.sh
@@ -4,7 +4,8 @@ termux-wake-lock
 
 . /data/data/com.termux/files/home/hottub-aws.sh
 
-while sleep 120; do
-timeout --signal=9 20 /data/data/com.termux/files/home/balboa_worldwide_app/bin/bwa_aws_iot_thing \
+while true; do
+    timeout --signal=9 20 /data/data/com.termux/files/home/balboa_worldwide_app/bin/bwa_aws_iot_thing \
 	| tee -a  /data/data/com.termux/files/home/bwa_aws_iot_thing.log 2>&1
+    sleep 120
 done
