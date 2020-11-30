395792d828905a3dedc7eede766a0615e6b9389e
David Kimdon <dkimdon@gmail.com>
2020-11-29 21:53:53 -0800

    Capture stderr as well as stdout.

--- termux-loop.sh
+++ termux-loop.sh
@@ -5,7 +5,7 @@ termux-wake-lock
 . /data/data/com.termux/files/home/hottub-aws.sh
 
 while true; do
-    timeout --signal=9 20 /data/data/com.termux/files/home/balboa_worldwide_app/bin/bwa_aws_iot_thing \
+    timeout --signal=9 20 /data/data/com.termux/files/home/balboa_worldwide_app/bin/bwa_aws_iot_thing 2>&1 \
 	| tee -a  /data/data/com.termux/files/home/bwa_aws_iot_thing.log 2>&1
     sleep 120
 done
